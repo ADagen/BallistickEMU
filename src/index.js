0bd35f71ec3f7a70de228c649181d5ce836c4881
Zaseth <hahano>
2019-12-03 21:39:13 +0100

    Load in config on entrypoint

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ const serverId = parseInt(process.argv[2])
 if (!serverId) {
   throw new Error('You must specify a valid server id.')
 }
+
+const config = require('../config')[serverId]
+
+if (!config) {
+  throw new Error('Missing config for specified server id.')
+}
+
+global.config = config
