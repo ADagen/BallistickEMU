8d9a66663d319fcf4ac6661abb42afa07c675e95
Zaseth <hahano>
2019-12-04 15:49:39 +0100

    Added max to config

--- config/index.js
+++ config/index.js
@@ -6,11 +6,13 @@
 module.exports = {
   100: {
     host: '127.0.0.1',
-    port: 1138
+    port: 1138,
+    max: 200
   },
 
   101: {
     host: '127.0.0.1',
-    port: 1139
+    port: 1139,
+    max: 100
   }
 }
