9397f98d1f6b189add257eaf40ce407a145117d3
Zaseth <hahano>
2019-12-04 18:42:07 +0100

    Add timeout to config

--- config/index.js
+++ config/index.js
@@ -7,12 +7,14 @@ module.exports = {
   100: {
     host: '127.0.0.1',
     port: 1138,
-    max: 200
+    max: 200,
+    timeout: 66000
   },
 
   101: {
     host: '127.0.0.1',
     port: 1139,
-    max: 100
+    max: 100,
+    timeout: 66000
   }
 }
