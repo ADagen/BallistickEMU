2fb5642924cd3e554a61058c9ba00e3559c42e38
Zaseth <hahano>
2019-12-04 21:32:11 +0100

    Remove generateClientId, this is not the good way

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,22 +1,6 @@
 'use strict'
 
 /**
- * Generates a random client id
- * @exports
- * @param {Object} clients
- * @returns {Number}
- */
-exports.generateClientId = function generateClientId(clients) {
-  let localID = 0
-
-  do {
-    localID = Math.floor(Math.random() * (900) + 100) // 100-999
-  } while (!!clients[localID]) // Must not exist
-
-  return localID
-}
-
-/**
  * Create a client id by incrementing the last client id
  * @exports
  * @param {Object} clients
