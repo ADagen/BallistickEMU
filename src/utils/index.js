384fb4886bfbc6bb2c85863448a19b2108de69b3
Zaseth <hahano>
2019-12-04 16:03:22 +0100

    Renamed to client id

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,12 @@
 'use strict'
 
 /**
- * Generates a random local ID
+ * Generates a random client id
  * @exports
  * @param {Object} clients
  * @returns {Number}
  */
-exports.generateLocalID = function generateLocalID(clients) {
+exports.generateClientId = function generateClientId(clients) {
   let localID = 0
 
   do { localID = Math.floor(Math.random() * (900) + 100) } while (!!clients[localID])
