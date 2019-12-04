e26274810991850aff5262e406cdc2f8e5ff4b26
Zaseth <hahano>
2019-12-04 10:58:20 +0100

    Added clients property to server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
 'use strict'
+
+/**
+ * @exports
+ * @class
+ */
+module.exports = class Server {
+  /**
+   * @constructor
+   */
+  constructor() {
+    /**
+     * The connected clients
+     * @type {Object}
+     */
+    this.clients = {}
+  }
+}
